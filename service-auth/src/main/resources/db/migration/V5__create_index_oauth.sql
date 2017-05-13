a247a9c2faeb1742ae00a1297dcdae25d34e7198
Imre Nagi <imre.nagi2812@gmail.com>
2017-05-13 12:57:02 -0700

    Add indexing for oauth tables

---
+++ service-auth/src/main/resources/db/migration/V5__create_index_oauth.sql
@@ -0,0 +1,12 @@
+CREATE INDEX token_id_index ON oauth_access_token (token_id);
+CREATE INDEX authentication_id_index ON oauth_access_token (authentication_id);
+CREATE INDEX refresh_token_index ON oauth_access_token (refresh_token);
+CREATE INDEX user_name_index ON oauth_access_token (user_name);
+CREATE INDEX client_id_index ON oauth_access_token (client_id);
+
+CREATE INDEX token_id_index ON oauth_client_token(token_id);
+CREATE INDEX authentication_id_index ON oauth_client_token (authentication_id);
+
+CREATE INDEX token_id_index ON oauth_refresh_token (token_id);
+
+CREATE INDEX client_id_index ON oauth_client_details(client_id);
