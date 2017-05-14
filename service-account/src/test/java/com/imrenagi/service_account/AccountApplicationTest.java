ec93e25c9ec545a66f8a9883b09f75927101c987
Imre Nagi <imre.nagi2812@gmail.com>
2017-05-14 13:24:23 -0700

    More unit test for account service

---
+++ service-account/src/test/java/com/imrenagi/service_account/AccountApplicationTest.java
@@ -0,0 +1,23 @@
+package com.imrenagi.service_account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by imrenagi on 5/14/17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AccountApplication.class)
+@WebAppConfiguration
+public class AccountApplicationTest {
+
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
